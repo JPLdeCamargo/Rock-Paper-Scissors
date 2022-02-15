641f9635d2b44e48f1b65ecee0ba90a0ed348e18
JPLdeCamargo <jplopescamargo@gmail.com>
2022-02-15 18:08:05 -0300

    added basic js

--- script.js
+++ script.js
@@ -1,4 +1,51 @@
+const playerScore = document.querySelector("#playerScore")
+const computerScore = document.querySelector("#computerScore")
+function clicked(playerChoice){
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(playerChoice, computerChoice);
+    console.log(winner)
+    changeScore(winner);
+}
 
-function clicked(){
-    a
+function getComputerChoice(){
+    let computerNumChoice = Math.floor(Math.random() * 3);
+    let computerChoice = '';
+    switch (computerNumChoice){
+        case 0:
+            computerChoice = 'rock';
+            break;
+        case 1:
+            computerChoice = 'paper';
+            break;
+        case 2:
+            computerChoice = 'scissor';
+            break;
+    }
+    return computerChoice;
+}
+
+function getWinner(playerChoice, computerChoice){
+    console.log(`${playerChoice}, ${computerChoice}`)
+    if (playerChoice === computerChoice){
+        return 'draw';
+    }
+    if ((playerChoice === 'rock' && computerChoice === 'scissor') ||
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissor' && computerChoice === 'paper')){
+            return 'player';
+    }
+    return 'computer';
+}
+
+function changeScore(winner){
+    if (winner === 'player'){
+        let intPlayerScore = parseInt(playerScore.textContent)
+        intPlayerScore++;
+        playerScore.textContent = `${intPlayerScore}`
+    }
+    else if (winner === 'computer'){
+        let intComputerScore = parseInt(computerScore.textContent)
+        intComputerScore++;
+        computerScore.textContent = `${intComputerScore}`
+    }
 }
